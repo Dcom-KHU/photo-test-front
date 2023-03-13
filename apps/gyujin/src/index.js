c6b70b9cdfec725d962255a1e0a1014cda4ed247
bluejoyq <bluejoy32@gmail.com>
2023-03-13 20:09:44 +0900

    feat: api 제작

--- apps/gyujin/src/index.js
+++ apps/gyujin/src/index.js
@@ -1,3 +1,28 @@
+const api = {
+  getPhotos: async () => {
+    const response = await fetch('/api/photos');
+    const body = await response.json();
+    return body;
+  },
+  getSecretPhotos: async ({ accessToken }) => {
+    const response = await fetch('/api/photos/secret', {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    const body = await response.json();
+    return body;
+  },
+  postLogin: async ({ username, email }) => {
+    const response = await fetch('/api/auth/login', {
+      method: 'POST',
+      body: JSON.stringify({ username, email }),
+    });
+    const body = await response.json();
+    return body.access_token;
+  },
+};
+
 class App {
   constructor({ initialState }) {
     this.state = initialState;
